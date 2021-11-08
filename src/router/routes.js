f045081c016c6b8c0a53ebfb08f52e8a0a4ac815
KubraGoktas <kbrgkts1999@gmail.com>
2021-11-08 23:52:21 +0300

    router ve reactstrap eklendi

---
+++ src/router/routes.js
@@ -0,0 +1,12 @@
+import AboutMe from "../pages/AboutMe";
+
+export const routes = [
+    {
+        page:'AboutMe',
+        component: <AboutMe />
+    },
+    {
+        page: '/',
+        component: <AboutMe />
+    }
+]
