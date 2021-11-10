f8f94384e538f454e13c351b6a2eefe5195459fd
KubraGoktas <kbrgkts1999@gmail.com>
2021-11-10 03:04:31 +0300

    form eklendi

--- src/pages/ContactMe.js
+++ src/pages/ContactMe.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Col, Row } from 'reactstrap'
+import { Button, Col, Input, Row } from 'reactstrap'
 import me from '../assets/images/me.jpg'
 
 const ContactMe = () => {
@@ -7,16 +7,31 @@ const ContactMe = () => {
         <div>
             <div class="parallax"
             >
-                <div style={{ height: '500px' }}>
-
+                <div style={{ height: '500px', padding: 200 }}>
+                    <Row className='contact-title'>
+                    Contact Me
+                    </Row>
+                    <Row>
+                        <Col sm='12' md='6'>
+                            <Input className='input' placeholder='Name' />
+                        </Col>
+                        <Col sm='12' md='6'>
+                            <Input className='input' placeholder='Surname' />
+                        </Col>
+                    </Row>
                     <Row >
-                        <Col>
-                            form
+                        <Col sm='12' md='12'>
+                            <Input className='input' placeholder='Message' />
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col sm='12' md='6'>
+                            <Button style={{ width: 200, backgroundColor: 'rgba(255,255,255,.55)', color: 'gray' }}>Send</Button>
                         </Col>
                     </Row>
                 </div>
             </div>
-            
+
         </div>
     )
 }
